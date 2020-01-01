036542b1493a101438e6ecf6c653a82f377d4ff5
petrero <petrero@gmail.com>
2020-01-01 21:51:17 +0200

    14.4.Fixtures: Seeding Dummy Data! {Creating Multiple Articles}
    Creating Multiple Articles
    First, we're going to do it the easy... but, kinda boring way. A for loop! Say: for  = 0;  < 10; ++. And, all the way at the bottom, add the ending curly brace. We need to call persist() in the loop, but we only need to call flush() once at the end.
    
    Cool! Try it again:
    
    php bin/console doctrine:fixtures:load
    Then, refresh! Awesome! Except that the articles are still totally boring and identical... we'll talk about that in the next chapter.

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -8,10 +8,11 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends Fixture {
   public function load(ObjectManager $manager) {
-	  $article = new Article();
-	  $article->setTitle('Why Asteroids Taste Like Bacon')
-		  ->setSlug('why-asteroids-taste-like-bacon-'.rand(100, 999))
-		  ->setContent(<<<EOF
+  	for($i=0; $i<=10; $i++) {
+		  $article = new Article();
+		  $article->setTitle('Why Asteroids Taste Like Bacon')
+			  ->setSlug('why-asteroids-taste-like-bacon-'.rand(100, 999))
+			  ->setContent(<<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
@@ -27,18 +28,19 @@ strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lo
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
 EOF
-		  );
+			  );
 
-	  // publish most articles
-	  if (rand(1, 10) > 2) {
-		  $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+		  // publish most articles
+		  if (rand(1, 10) > 2) {
+			  $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+		  }
+
+		  $article->setAuthor('Mike Ferengi')
+			  ->setHeartCount(rand(5, 100))
+			  ->setImageFilename('asteroid.jpeg');
+
+			$manager->persist($article);
 	  }
-
-	  $article->setAuthor('Mike Ferengi')
-		  ->setHeartCount(rand(5, 100))
-		  ->setImageFilename('asteroid.jpeg');
-
-		$manager->persist($article);
 	  $manager->flush();
   }
 }
