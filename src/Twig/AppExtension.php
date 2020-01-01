e41fcbf89bb7b2f49eee275975b1b964395809a5
petrero <petrero@gmail.com>
2020-01-01 18:16:48 +0200

    8.6.Service Subscriber: Lazy Performance{Understanding getSubscribedServices()}
    There is a better way for creating lazy-Loaded Twig extensions since Twig v1.26. First of all, create a separate class, e.g. AppRuntime, that implements RuntimeExtensionInterface and inject MarkdownHelper object there. Also, move processMarkdown() method there
    And then, in AppExtension, remove MarkdownHelper at all and point the cached_markdown filter to [AppRuntime::class, 'processMarkdown'] instead
    That's it! Now our Twig extension does not have any direct dependencies, and AppRuntime object will be created only when cached_markdown is called.
    Here's the super-duper-important takeaway: I want you to use normal dependency injection everywhere - just pass each service you need through the constructor, without all this fancy service-subscriber stuff.
    But then, in just a couple of places in Symfony, the main ones being Twig extensions, event subscribers and security voters - a few topics we'll talk about in the future - you should consider using a service subscriber instead to avoid a performance hit.

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -9,33 +9,14 @@ use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
-class AppExtension extends AbstractExtension implements ServiceSubscriberInterface {
-	/**
-	 * @var ContainerInterface
-	 */
-	private $container;
-
-	public function __construct(ContainerInterface $container) {
-		$this->container = $container;
-	}
+class AppExtension extends AbstractExtension {
 
 	public function getFilters(): array {
       return [
         // If your filter generates SAFE HTML, you should add a third
         // parameter: ['is_safe' => ['html']]
         // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-        new TwigFilter('cached_markdown', [$this, 'processMarkdown']),
+        new TwigFilter('cached_markdown', [AppRuntime::class, 'processMarkdown'])
       ];
   }
-
-  public function processMarkdown($value) {
-    return $this->container
-	    ->get(MarkdownHelper::class)->parse($value);
-  }
-
-	public static function getSubscribedServices() {
-		return [
-			MarkdownHelper::class
-		];
-	}
 }
