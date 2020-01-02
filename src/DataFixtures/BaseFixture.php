6e4acb45bc0a8d6de5490b883e28543192e51cb9
petrero <petrero@gmail.com>
2020-01-02 09:10:01 +0200

    15.3.Using Faker for Seeding Data{Setting up Faker}
    Setting up Faker
    When that finishes, head back to its docs so we can see how to use it. Ok: we just need to say $faker = Faker\Factory::create(). Open our BaseFixture class: let's setup Faker in this, central spot. Create a new protected $faker property.
    And down below, I'll say, $this->faker = and look for a class called Factory from Faker, and ::create()
    We should also add some PHPDoc above the property to help PhpStorm know what type of object it is. Hold Command - or Ctrl - and click the create() method: let's see what this returns exactly. Apparently, it returns a Generator.
    
    Cool! Above the property, add /** @var Generator */ - the one from Faker
    Perfect! Now, using Faker will be as easy as pie! Specifically, eating pie, cause, that's super easy.

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -4,16 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 abstract class BaseFixture extends Fixture {
 	/** @var ObjectManager */
 	private $manager;
 
+	/** @var Generator */
+	protected $faker;
+
 	abstract protected function loadData(ObjectManager $em);
 
 	public function load(ObjectManager $manager) {
 		$this->manager = $manager;
-
+		$this->faker = Factory::create();
 		$this->loadData($manager);
 	}
 
